0bf91561a2ee907f0577be97d112124832309f83
musheer243 <bilalaijazkhanam@gmail.com>
2025-02-06 15:38:45 +0530

    create post updated

--- src/components/dashboard/CreatePostPage.js
+++ src/components/dashboard/CreatePostPage.js
@@ -1,21 +1,29 @@
-import React, { useState, useEffect } from "react";
-import "../../styles/CreatePostPage.css";
-import { API_BASE_URL } from "../../config";
+import React, { useState, useEffect, useRef } from 'react';
+import '../../styles/CreatePostPage.css';
+import { IoImagesOutline } from 'react-icons/io5';
+import { CiCamera } from 'react-icons/ci';
+import { FaCircleDot } from "react-icons/fa6";
+import { MdAddPhotoAlternate } from 'react-icons/md';
+import { API_BASE_URL } from '../../config';
 
 function CreatePostPage() {
-  const [title, setTitle] = useState("");
+  const [title, setTitle] = useState('');
   const [mediaFiles, setMediaFiles] = useState([]);
-  const [content, setContent] = useState("");
+  const [content, setContent] = useState('');
   const [categories, setCategories] = useState([]);
   const [filteredCategories, setFilteredCategories] = useState([]);
   const [selectedCategoryId, setSelectedCategoryId] = useState(null);
-  const [searchKeyword, setSearchKeyword] = useState("");
+  const [searchKeyword, setSearchKeyword] = useState('');
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [currentMediaIndex, setCurrentMediaIndex] = useState(0);
+  const [isCameraActive, setIsCameraActive] = useState(false);
+  const [closeFriendsOnly, setCloseFriendsOnly] = useState(false);
+  const [scheduledDate, setScheduledDate] = useState('');
+  const videoRef = useRef(null);
+  const streamRef = useRef(null);
+  const titleInputRef = useRef(null);
+  const jwtToken = localStorage.getItem('jwtToken');
 
-  const jwtToken = localStorage.getItem("jwtToken"); // Replace this with your authentication mechanism
-
-  // Fetch all categories
   useEffect(() => {
     const fetchCategories = async () => {
       try {
@@ -27,21 +35,18 @@ function CreatePostPage() {
 
         if (response.ok) {
           const data = await response.json();
-
-          // Sort categories alphabetically by categoryTitle
-          const sortedCategories = data.sort((a, b) => {
-            if ((a?.categoryTitle || "").toLowerCase() < (b?.categoryTitle || "").toLowerCase()) return -1;
-            if ((a?.categoryTitle || "").toLowerCase() > (b?.categoryTitle || "").toLowerCase()) return 1;
-            return 0;
-          });
-
+          const sortedCategories = data.sort((a, b) =>
+            (a?.categoryTitle || '').localeCompare(b?.categoryTitle || '', undefined, {
+              sensitivity: 'base',
+            })
+          );
           setCategories(sortedCategories);
-          setFilteredCategories(sortedCategories); // Show all categories initially
+          setFilteredCategories(sortedCategories);
         } else {
-          console.error("Failed to fetch categories");
+          console.error('Failed to fetch categories');
         }
       } catch (error) {
-        console.error("Error fetching categories:", error);
+        console.error('Error fetching categories:', error);
       }
     };
 
@@ -49,44 +54,42 @@ function CreatePostPage() {
   }, [jwtToken]);
 
   useEffect(() => {
-    if (searchKeyword.trim() === "") {
-      setFilteredCategories(categories); // Already sorted
-    } else {
-      setFilteredCategories(
-        categories
-          .filter((category) =>
-            (category?.categoryTitle || "").toLowerCase().includes(searchKeyword.toLowerCase())
+    setFilteredCategories(
+      searchKeyword.trim() === ''
+        ? categories
+        : categories.filter((category) =>
+            category?.categoryTitle?.toLowerCase().includes(searchKeyword.toLowerCase())
           )
-          .sort((a, b) => {
-            if ((a?.categoryTitle || "").toLowerCase() < (b?.categoryTitle || "").toLowerCase()) return -1;
-            if ((a?.categoryTitle || "").toLowerCase() > (b?.categoryTitle || "").toLowerCase()) return 1;
-            return 0;
-          })
-      );
-    }
+    );
   }, [searchKeyword, categories]);
 
-  // Handle post submission
-  const handlePostSubmit = async () => {
+  const handlePostSubmit = async (event) => {
+    event.preventDefault();
+
     if (!selectedCategoryId) {
-      alert("Please select a category.");
+      alert('Please select a category.');
       return;
     }
 
     if (!title || !content) {
-      alert("Title and content are required.");
+      alert('Title and content are required.');
       return;
     }
 
     const formData = new FormData();
-    formData.append("postDto", JSON.stringify({ title, content }));
-    mediaFiles.forEach((file) => formData.append("mediaFiles", file));
+    formData.append('postDto', JSON.stringify({ 
+      title, 
+      content, 
+      closeFriendsOnly, 
+      scheduledDate: scheduledDate ? new Date(scheduledDate).toISOString() : null 
+    }));
+    mediaFiles.forEach((file) => formData.append('mediaFiles', file));
 
     try {
-      const response = await fetch(
-`${API_BASE_URL}/api/user/1/category/${selectedCategoryId}/posts/with-media`,
+      const response = await fetch(`
+        ${API_BASE_URL}/api/user/1/category/${selectedCategoryId}/posts/with-media`,
         {
-          method: "POST",
+          method: 'POST',
           headers: {
             Authorization: `Bearer ${jwtToken}`,
           },
@@ -95,18 +98,18 @@ function CreatePostPage() {
       );
 
       if (response.ok) {
-        // const data = await response.json();
-        alert("Post created successfully!");
-        setTitle("");
-        setContent("");
+        alert('Post created successfully!');
+        setTitle('');
+        setContent('');
         setMediaFiles([]);
         setSelectedCategoryId(null);
+        setCloseFriendsOnly(false);
+        setScheduledDate('');
       } else {
-        // const errorResponse = await response.json();
-        alert("Failed to create post. Please try again.");
+        alert('Failed to create post. Please try again.');
       }
     } catch (error) {
-      alert("An error occurred while creating the post.");
+      alert('An error occurred while creating the post.');
     }
   };
 
@@ -122,59 +125,161 @@ function CreatePostPage() {
     }
   };
 
+        // CAMERA
+
+  const handleCameraStart = async () => {
+
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "user" } });
+
+      streamRef.current = stream; // Store the stream reference
+      setIsCameraActive(true); // Triggers re-render to show <video>
+  
+      console.log("Stream received:", stream);
+      
+      if (videoRef.current) {
+        console.log("Video Element Found:", videoRef.current);
+        videoRef.current.srcObject = stream;
+        videoRef.current.onloadeddata = () => {
+          videoRef.current.play();
+        };
+      } else {
+        console.error("Video element not found.");
+      }
+
+    } catch (error) {
+      alert("Error accessing the camera. Please check permissions.");
+      console.error("Camera error:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (isCameraActive && videoRef.current) {
+      console.log("Applying stream to video element:", videoRef.current);
+      videoRef.current.srcObject = streamRef.current;
+      videoRef.current.onloadeddata = () => {
+        videoRef.current.play();
+      };
+    }
+  }, [isCameraActive]);
+
+  const handleCaptureImage = () => {
+    if (videoRef.current) {
+      const canvas = document.createElement("canvas");
+      const context = canvas.getContext("2d");
+      canvas.width = videoRef.current.videoWidth;
+      canvas.height = videoRef.current.videoHeight;
+
+      // Flip the canvas horizontally
+    context.translate(canvas.width, 0);
+    context.scale(-1, 1); // Mirrors the image
+
+      context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+
+      canvas.toBlob((blob) => {
+        if (blob) {
+          const file = new File([blob], "captured-image.jpg", { type: "image/jpeg" });
+          setMediaFiles((prev) => [...prev, file]);
+          handleCloseCamera();
+        }
+      });
+    }
+  };
+
+  const handleCloseCamera = () => {
+    if (streamRef.current) {
+      streamRef.current.getTracks().forEach((track) => track.stop());
+    }
+    setIsCameraActive(false);
+  };
+
+  
+
   return (
     <div className="create-post-wrapper">
       <div className="create-post-container">
         <h2>Create a Post</h2>
 
-        <input
-          type="text"
-          placeholder="Enter the title of the post"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className="input-field"
-        />
+        <div className="title-input-container">
+          <input
+            ref={titleInputRef}
+            type="text"
+            placeholder="Enter the title of the post"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="input-field"
+          />
 
-<div className="media-preview-container">
-  <div className="media-preview">
-    {mediaFiles.length > 0 && mediaFiles[currentMediaIndex] ? (
-      mediaFiles[currentMediaIndex].type.startsWith("image/") ? (
-        <img
-          src={URL.createObjectURL(mediaFiles[currentMediaIndex])}
-          alt={`Uploaded Media ${currentMediaIndex + 1}`}
-          className="media-preview-content"
-        />
-      ) : (
-        <video
-          controls
-          src={URL.createObjectURL(mediaFiles[currentMediaIndex])}
-          className="media-preview-content"
-        />
-      )
-    ) : (
-      <span className="media-placeholder"></span>
-    )}
-    {mediaFiles.length > 1 && (
-      <div className="media-navigation">
-        <button onClick={handlePreviousMedia}>{"<"}</button>
-        <button onClick={handleNextMedia}>{">"}</button>
-      </div>
-    )}
-  </div>
-  <input
-    type="file"
-    multiple
-    accept="image/,video/"
-    onChange={(e) => {
-      const files = Array.from(e.target.files);
-      if (files.length > 0) {
-        setMediaFiles(files);
-        setCurrentMediaIndex(0);
-      }
-    }}
-    className="input-field"
-  />
-</div>
+        <div className="media-preview-container">
+          <div className="media-preview">
+            {mediaFiles.length > 0 && mediaFiles[currentMediaIndex] ? (
+              mediaFiles[currentMediaIndex].type.startsWith('image/') ? (
+                <img
+                  src={URL.createObjectURL(mediaFiles[currentMediaIndex])}
+                  alt={`Uploaded Media ${currentMediaIndex + 1}`}
+                  className="media-preview-content"
+                />
+              ) : (
+                <video
+                  controls
+                  src={URL.createObjectURL(mediaFiles[currentMediaIndex])}
+                  className="media-preview-content"
+                />
+              )
+            ) : (
+              <label 
+                htmlFor="file-upload" 
+                style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', cursor: 'pointer' }}
+              >
+                <IoImagesOutline size={50} />
+                <p style={{ margin: 0 }}>Upload Image & Videos.</p>
+              </label>
+            )}
+
+            <input
+              id="file-upload"
+              type="file"
+              multiple
+              accept="image/,video/"
+              onChange={(e) => {
+                const files = Array.from(e.target.files);
+                if (files.length > 0) {
+                  setMediaFiles((prev) => [...prev, ...files]);
+                  setCurrentMediaIndex(0);
+                }
+              }}
+              style={{ display: 'none' }}
+            />
+
+            {mediaFiles.length > 1 && (
+              <div className="media-navigation">
+                <button onClick={handlePreviousMedia}>{'<'}</button>
+                <button onClick={handleNextMedia}>{'>'}</button>
+              </div>
+            )}
+          </div>
+
+          <div className="camera-container">
+            <div className="icon-controls">
+              <CiCamera size={30} style={{ cursor: "pointer", marginRight: "10px" }} onClick={handleCameraStart} />
+              {mediaFiles.length > 0 && (
+                <MdAddPhotoAlternate
+                  size={30}
+                  style={{ cursor: "pointer", marginLeft: "10px" }}
+                  onClick={() => document.getElementById("file-upload").click()}
+                />
+              )}
+            </div>
+
+            {isCameraActive && (
+              <div className="camera-fullscreen">
+                <video ref={videoRef} className="camera-video" autoPlay playsInline />
+                <FaCircleDot size={50} className="capture-btn" onClick={handleCaptureImage} />
+                <button className="close-btn" onClick={handleCloseCamera}>X</button>
+              </div>
+            )}
+          </div>
+        </div>
 
         <textarea
           placeholder="Enter the content of the post"
@@ -184,15 +289,15 @@ function CreatePostPage() {
           className="textarea-field"
         />
 
+{!isCameraActive && (
         <div className="dropdown-container">
           <div
             className="dropdown-header"
             onClick={() => setIsDropdownOpen(!isDropdownOpen)}
           >
             {selectedCategoryId
-              ? categories.find((cat) => cat.categoryId === selectedCategoryId)
-                  ?.categoryTitle
-              : "Select a category"}
+              ? categories.find((cat) => cat.categoryId === selectedCategoryId)?.categoryTitle
+              : 'Select a category'}
           </div>
           {isDropdownOpen && (
             <div className="dropdown-menu">
@@ -219,11 +324,38 @@ function CreatePostPage() {
             </div>
           )}
         </div>
+    )}
+
+        <div className="close-friends-only-container">
+          <label htmlFor="close-friends-only">
+            <input
+              type="checkbox"
+              id="close-friends-only"
+              name="close-friends-only"
+              checked={closeFriendsOnly}
+              onChange={(e) => setCloseFriendsOnly(e.target.checked)}
+            />
+             Close Friends Only
+          </label>
+        </div>
+
+        <div className="schedule-post-container">
+          <label htmlFor="scheduled-date">Schedule Post:</label>
+          <input
+            type="datetime-local"
+            id="scheduled-date"
+            name="scheduled-date"
+            value={scheduledDate}
+            onChange={(e) => setScheduledDate(e.target.value)}
+            className="input-field"
+          />
+        </div>
 
         <button onClick={handlePostSubmit} className="submit-btn">
           Create Post
         </button>
       </div>
+    </div>
     </div>
   );
 }
