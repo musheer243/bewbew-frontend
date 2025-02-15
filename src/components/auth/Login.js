417c63fdb0c72892ab747bca8811f48262130331
musheer243 <bilalaijazkhanam@gmail.com>
2025-02-15 14:21:16 +0530

    login.css changed to Login.css

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../../styles/login.css";
+import "../../styles/Login.css";
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
